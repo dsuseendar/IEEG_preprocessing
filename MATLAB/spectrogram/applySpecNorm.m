ee8a1f4907a6083d8c115a84b47d9277ed4ac6c6
Kumar <sdurai30@evnode.mit.edu>
2024-11-18 17:20:08 -0500

    modularizing spectrogram normalization

--- MATLAB/spectrogram/applySpecNorm.m
+++ MATLAB/spectrogram/applySpecNorm.m
@@ -1,2 +1,11 @@
-function applySpecNorm(spec,meanFreqChanIn)
+function specMean =  applySpecNorm(spec,meanFreqChanIn)
+
+    assert(length(spec)==size(meanFreqChanIn,1),"Channel number mismatch");
+    
+    for iChan = 1:length(spec)
+        spec2Analyze = spec{iChan}; % Spectrogram to analyze   
+        meanFreq = meanFreqChanIn(iChan, :); 
+        specMean{iChan} = (squeeze(mean(spec2Analyze ./ reshape(meanFreq, 1, 1, []), 1)))'';
+       
+    end
 end
