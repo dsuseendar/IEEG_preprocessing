507e88dcea52985575ca0639b42ffd123cf95e0e
Kumar <sdurai30@evnode.mit.edu>
2024-11-22 15:02:09 -0500

    changes to spectrogram plotting

--- MATLAB/spectrogram/plotSpec.m
+++ MATLAB/spectrogram/plotSpec.m
@@ -9,6 +9,7 @@ function plotSpec(spec,tw,freq,args)
         args.etw = tw;
         args.title = '';
         args.cval = [0.7 1.4];
+        
     end
 
     tspec = linspace(tw(1), tw(2), size(spec, 1)); % Time vector for spectrograms
@@ -18,14 +19,17 @@ function plotSpec(spec,tw,freq,args)
         spec2plot = squeeze(spec);
     end
     imagesc(tspec, [], spec2plot');
-    xlim(args.etw);
+    
     set(gca,'YTick',1:args.f_interal:length(freq));
     set(gca,'YTickLabels',round(freq(1:args.f_interal:end)));
     set(gca,'FontSize',args.font_size);
-    title(args.title);
+    set(gca, 'YDir', 'normal');
+    xlim(args.etw);
+   
+    title(args.title,'Interpreter','none');
     if(args.isdb)
-        caxis(20.*log10(cval))
+        caxis(20.*log10(args.cval))
     else
-        caxis(cval)
+        caxis(args.cval)
     end
 end
