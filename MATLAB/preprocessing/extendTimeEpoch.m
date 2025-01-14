8d8ffd2282de41f122976bd76099fb9ccdb5d858
Kumar <dsuseendar@gmail.com>
2025-01-14 10:31:18 -0500

    Changed module from class object to function

--- MATLAB/preprocessing/extendTimeEpoch.m
+++ MATLAB/preprocessing/extendTimeEpoch.m
@@ -1,20 +1,15 @@
-function ieegStructNew = extendTimeEpoch(ieegStruct,newTimeEpoch)
+function ieegPad = extendTimeEpoch(ieegData,sigLen)
 %EXTENDTIMEEPOCH 
 warning off;
-padTimeArray = [0.1 0.2 0.3 0.4 0.5];
-ieegData = ieegStruct.data;
-timeEpoch = ieegStruct.tw;
-fs = ieegStruct.fs;
-padNumArray = padTimeArray*fs;
-%time2pad = (newTimeEpoch(2)-newTimeEpoch(1))/(timeEpoch(2)-timeEpoch(1));
-%time2pad = (newTimeEpoch(2)-newTimeEpoch(1))/padTime;
-sigLen = (newTimeEpoch(2)-newTimeEpoch(1))*fs;
+padTimeArray = 50:50:500;
+
+
 for iChan = 1:size(ieegData,1)
     ieegChan = squeeze(ieegData(iChan, :, :));
     ieegChanPad = [];
     for iTrial = 1:size(ieegChan, 1)
         padChoose = randsample(padTimeArray,1);
-        time2pad = ((newTimeEpoch(2)-newTimeEpoch(1))/padChoose);
+        time2pad = sigLen/padChoose;
         selectTrials = setdiff(1:size(ieegChan, 1),iTrial);
         randTrials = datasample(selectTrials, ceil(time2pad) - 1, 'Replace', false);
 
@@ -43,8 +38,5 @@ for iChan = 1:size(ieegData,1)
     ieegPad(iChan,:,:) = ieegChanPad;
 end
 
-ieegStructNew = ieegStruct;
-ieegStructNew.data = ieegPad;
-ieegStructNew.tw = newTimeEpoch;
 
 
